df3ce7f282abbf8a1fb56bee2c584156402c5590
Stingos <Justmo10@gmail.com>
2025-10-01 01:23:14 -0400

    fix issue with deployment

--- webpack.config.js
+++ webpack.config.js
@@ -2,38 +2,42 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    publicPath: process.env.NODE_ENV === 'production' ? '/battleship/' : '/',
-    clean: true,
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  
+  return {
+    mode: argv.mode || "development",
+    entry: "./src/index.js",
+    output: {
+      filename: "main.js",
+      path: path.resolve(__dirname, "dist"),
+      publicPath: isProduction ? '/battleship/' : '/',
+      clean: true,
+    },
+    devtool: isProduction ? "source-map" : "eval-source-map",
+    devServer: {
+      watchFiles: ["./src/template.html"],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/template.html",
+      }),
     ],
-  },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.html$/i,
+          loader: "html-loader",
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+      ],
+    },
+  };
 };
